c3f147812be56e3a2cd441b5e89bf6420f433e0e
Chris <chris@raroque.org>
2013-12-29 22:34:32 -0600

    Initial Comit for Guessing Game
    Guessing Game initial comit

--- My First Project/main.m
+++ My First Project/main.m
@@ -13,9 +13,24 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        int answer = 0;
+        int guess = 0;
+        int turn = 0;
         
+        answer = arc4random() % 100 + 1;
+        while (guess != answer) {
+        NSLog(@"Guess number %i, Enter a number between 1 and 100", turn);
+            turn++;
+        scanf("%i", &guess);
+        if (guess > answer) {
+            NSLog(@"Lower!");
+        } else if (guess < answer){
+            NSLog(@"Higher!");
+        } else {
+            NSLog(@"Correct! The answer was %i", answer);
+        }
+    }
+        NSLog(@"It took you %i tries", turn);
     }
     return 0;
 }
