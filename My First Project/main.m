58c3e2f8af48936912378014331dbb8ec7cb99a5
Chris <IO@io.gateway.2wire.net>
2013-12-29 21:56:33 -0600

    Initial Commit

---
+++ My First Project/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  My First Project
+//
+//  Created by Chris on 12/29/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
